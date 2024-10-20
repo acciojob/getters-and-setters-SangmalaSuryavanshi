63d5cdb117cf845d9acb26ba76bfa8fdf584655c
SangmalaSuryavanshi <161364128+SangmalaSuryavanshi@users.noreply.github.com>
2024-10-20 19:37:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
 //complete this code
 class Person {}
 
-class Student extends Person {}
+class Person {
+    constructor(name, age) {
+        this._name = name; // Using a private variable to store name
+        this._age = age;   // Using a private variable to store age
+    }
 
-class Teacher extends Person {}
+    get name() {
+        return this._name;
+    }
+
+    set age(age) {
+        this._age = age;
+    }
+
+    get age() {
+        return this._age;
+    }
+}
+
+class Student extends Person {
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+class Teacher extends Person {
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
+
+// Example usage:
+let person = new Person("John", 25);
+console.log(person.name); // Output: John
+
+person.age = 30;
+console.log(person.age); // Output: 30
+
+let student = new Student("Alice", 22);
+student.study(); // Output: Alice is studying
+
+let teacher = new Teacher("Bob", 40);
+teacher.teach(); // Output: Bob is teaching
+
 
 // Do not change the code below this line
 window.Person = Person;
